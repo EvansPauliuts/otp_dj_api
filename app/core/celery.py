9611ee081e1daf0fe24b28833096f0f37c6d9e44
Evans <pauliutsevans@gmail.com>
2024-02-19 16:28:42 +0300

    Day 1 - 1

---
+++ app/core/celery.py
@@ -0,0 +1,25 @@
+import os
+
+from django.apps import apps, AppConfig
+from django.conf import settings
+from celery import Celery
+from decouple import config
+
+if not settings.configured:
+    environment = config('ENVIRONMENT')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'core.settings.{environment}')
+
+APP = Celery('core')
+
+
+class CeleryConfig(AppConfig):
+    name = 'core'
+    verbose_name = 'Celery Config'
+
+    def ready(self):
+        APP.config_from_object('django.conf:settings', namespace='CELERY')
+        installed_apps = [app_config.name for app_config in apps.get_app_configs()]
+        APP.autodiscover_tasks(installed_apps, force=True)
+
+    def tearDown(self):
+        pass
